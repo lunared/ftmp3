96e9b3239cb19e9852b08221bdf1f9783037b324
Nicholas Hydock <nhydock@gmail.com>
2016-09-02 18:01:45 -0400

    improve formatting and sorting of song titles

--- app.py
+++ app.py
@@ -21,27 +21,27 @@ FORMAT_MATCH = re.compile(r"\.(mp3|ogg|midi|mid)$")
 
 
 def get_songs(path):
-    filepath = f"{MUSIC_DIRECTORY}"
+    filepath = "{0}".format(MUSIC_DIRECTORY)
     if path:
-        filepath = f"{MUSIC_DIRECTORY}{path}"
+        filepath = "{0}{1}".format(MUSIC_DIRECTORY, path)
     files = [f for f in os.listdir(filepath) if FORMAT_MATCH.search(f)]
     #raise ValueError(files)
     songs = []
     for file in files:
-        songpath = f"{filepath}{file}"
+        songpath = "{0}{1}".format(filepath, file)
         #raise ValueError(songpath)
         song = tinytag.TinyTag.get(songpath)
         songs.append({
-            'artist':song.artist,
+            'artist':song.artist if song.artist else "",
             'title':song.title,
-            'track':song.track,
-            'album':song.album,
-            'length':f"{int(song.duration//60)}:{int(song.duration%60):02d}",
+            'track':song.track if song.track else "",
+            'album':song.album if song.album else "",
+            'length':"{0}:{1:02d}".format(int(song.duration//60), int(song.duration%60)),
             'path':file
         })
 
     return {
-        'songs': sorted(songs, key=lambda song: int(song['track'])),
+        'songs': sorted(songs, key=lambda song: (song['album'], int(song['track'] if song['track'] else 0), song['artist'], song['title'])),
         'path': path
     }
 
