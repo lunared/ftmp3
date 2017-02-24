29d047b1f396a1f49c8ddf1e1f5ffc3c0eda71eb
nhydock <nhydock@gmail.com>
2017-02-23 22:54:41 -0500

    Port should be bound to env

--- app.py
+++ app.py
@@ -3,6 +3,9 @@
 from src.app import app
 from src import views
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', 
+            port=os.environ.get('FTMP3_PORT', 5000))
 
