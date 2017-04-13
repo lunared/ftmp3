4e4390db6ed35de4fb7ad42579be5180a95bb96f
nhydock <nhydock@gmail.com>
2017-04-12 21:25:11 -0400

    Allow for case-insensitive checking of file formats.  Support m4a

--- src/settings.py
+++ src/settings.py
@@ -4,13 +4,13 @@ import os
 # Root directory that we scan for music from
 # Do not change this unless you're not using the docker-compose
 # It is preferred you use just change the volume mapping on the docker-compose.yml
-MUSIC_DIRECTORY = os.environ.get("FTMP3_MUSIC", "/media/Music")
+MUSIC_DIRECTORY = os.environ.get("FTMP3_MUSIC", r"/media/Music/")
 # Tells flask to serve the mp3 files
 # Typically you'd want nginx to do this instead, as this is an
 # easy way to cause concurrent response issues with flask
 SERVE_FILES = True
 # acceptable standard html5 compatible formats
-FORMAT_MATCH = re.compile(r"\.(mp3|ogg|midi|mid)$")
+FORMAT_MATCH = re.compile(r"(?i)\.(mp3|m4a|ogg)$")
 
 # number of directories upwards to limit recursive check for cover image
 COVER_IMG_RECURSION_LIMIT = 1
